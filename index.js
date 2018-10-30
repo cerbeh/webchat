29834ace4d63f0d6e5b977c76f85478ed632f84d
Martin Allgood <martintallgood@gmail.com>
2018-10-30 21:18:26 +0000

    Began work on router, added jsonwebtoken

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app =  express();
 
-const { port } = require('./config/environment');
+const { port, dbURI } = require('./config/environment');
+const routes = require('./config/routes');
 
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
+mongoose.connect(dbURI, {useNewUrlParser: true});
+mongoose.Promise = require('bluebird');
+
+app.use('/api', routes);
 
 app.listen(port, () => console.log(`listening on port ${[port]}`));
